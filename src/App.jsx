f02ee10508a9526b5c833d192761d723adef996b
Haziq Hapiz <zyqq.dev@gmail.com>
2023-03-29 16:45:02 +0800

    add register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import SignIn from "./components/pages/SignIn";
 import Home from "./components/pages/Home";
 import NotFound from "./components/pages/NotFound";
+import SignUp from "./components/pages/SignUp";
 
 import { Route, Routes } from "react-router-dom";
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
     </div>
