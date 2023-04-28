ed66f26bb13c63bd85da512520dea9138fe8b928
Haziq Hapiz <zyqq.dev@gmail.com>
2023-04-28 17:16:15 +0800

    add compressImage function

--- src/components/pages/NewItem.jsx
+++ src/components/pages/NewItem.jsx
@@ -14,15 +14,16 @@ export default function NewItem() {
 
     const state = useLocation().state;
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         setUserID(state.ID);
         event.preventDefault();
+        const compressedImage = await compressImage(image);
         const formData = new FormData();
         formData.append("name", name);
         formData.append("description", description);
         formData.append("price", price);
         formData.append("quantity", quantity);
-        formData.append("image", image);
+        formData.append("image", compressedImage);
         formData.append("userID", userID);
 
         axios.post(path.url + "api/item/create", formData, {
@@ -37,6 +38,42 @@ export default function NewItem() {
             console.error("Error:", error);
         });
     }
+
+    const compressImage = (file) => {
+      return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = (event) => {
+          const img = new Image();
+          img.src = event.target.result;
+          img.onload = () => {
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
+            const MAX_WIDTH = 800;
+            let width = img.width;
+            let height = img.height;
+            if (width > MAX_WIDTH) {
+              height *= MAX_WIDTH / width;
+              width = MAX_WIDTH;
+            }
+            canvas.width = width;
+            canvas.height = height;
+            ctx.drawImage(img, 0, 0, width, height);
+            canvas.toBlob(
+              (blob) => {
+                const compressedFile = new File([blob], file.name, {
+                  type: file.type,
+                  lastModified: Date.now(),
+                });
+                resolve(compressedFile);
+              },
+              file.type,
+              0.7
+            );
+          };
+        };
+      });
+    };
 
     return (
       <div>
