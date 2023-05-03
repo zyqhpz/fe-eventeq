6f8791877f8b1932571d08a9d8a8e30aa6ecbd2b
Haziq Hapiz <zyqq.dev@gmail.com>
2023-05-03 14:42:03 +0800

    update gitignore

--- src/components/utils/path.js
+++
@@ -1,8 +0,0 @@
-const isProduction = true;
-// if ifProduction is true, then the path will be the production path
-// if ifProduction is false, then the path will be the development path
-export default {
-  url: isProduction
-    ? "https://be-eventeq-production.up.railway.app/"
-    : "http://localhost:8080/",
-};
