ddef4aba1760f8386e138f3d4a48f56b3657b581
Haziq Hapiz <zyqq.dev@gmail.com>
2023-05-03 14:16:57 +0800

    add overlay when submitting

--- src/components/pages/NewItem.jsx
+++ src/components/pages/NewItem.jsx
@@ -21,12 +21,6 @@ export default function NewItem() {
 
     const handleSubmit = async (event) => {
         setLoading(true)
-
-        if (loading) {
-          processingSpinner();
-          console.log("loading");
-        }
-
         setUserID(state.ID);
         event.preventDefault();
         const formData = new FormData();
@@ -58,7 +52,6 @@ export default function NewItem() {
                 console.error("Error:", error);
               });
             setLoading(false);
-            console.log("done");
           }
         }, 2000);
     }
@@ -123,28 +116,10 @@ export default function NewItem() {
       });
     };
     
-    const processingSpinner = (
-      <div className="fixed inset-0 bg-gray-500 opacity-50 z-50">
-        <button type="button" className="bg-orange-500 ..." disabled>
-          <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
-          </svg>
-          Creating...
-        </button>
-      </div>
-    );
-
     return (
-      <div
-        className={loading ? "fixed inset-0 bg-gray-500 opacity-50 z-50" : ""}
-      >
-        {loading ? (
-                  <button type="button" className="bg-orange-500 fixed inset-0 " disabled>
-                    <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
-                    </svg>
-                    Creating...
-                  </button>) : (<></>)}
+      <div>
         <div>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit} className="gap-2">
             <label>
               Name:
               <input
@@ -189,7 +164,6 @@ export default function NewItem() {
               htmlFor="file-upload"
               className="relative block w-full h-48 mt-4 border-2 border-dashed border-gray-300 rounded-lg cursor-pointer hover:border-gray-400"
             >
-              Image:
               <div className="absolute top-0 left-0 w-full h-full opacity-0"></div>
               <div className="flex flex-col items-center justify-center w-full h-full bg-gray-200 hover:bg-gray-300">
                 <svg
@@ -222,11 +196,41 @@ export default function NewItem() {
             </label>
             <input
               type="submit"
-              className="bg-blue-400 p-12"
-              value="Add Images"
+              className="bg-orange-400 hover:bg-orange-500 border-orange-800 border-2 border-opacity-30 p-4 m-auto block mt-4 rounded-lg text-white font-bold"
+              value="Add new item"
             />
-            {/* <input type="submit" value="Submit" /> */}
           </form>
+          {loading ? (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <div className="absolute inset-0 bg-gray-400 opacity-50 z-40"></div>
+              <button
+                disabled
+                type="button"
+                className="text-white bg-orange-500 border-orange-800 border-2 border-opacity-40 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 inline-flex items-center inset-0 z-50"
+              >
+                <svg
+                  aria-hidden="true"
+                  role="status"
+                  className="inline w-4 h-4 mr-3 text-white animate-spin"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="#E5E7EB"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentColor"
+                  />
+                </svg>
+                Processing..
+              </button>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
         <br />
         {selectedImages.length > 0 &&
--- src/components/utils/path.js
+++ src/components/utils/path.js
@@ -1,4 +1,4 @@
-const isProduction = false;
+const isProduction = true;
 // if ifProduction is true, then the path will be the production path
 // if ifProduction is false, then the path will be the development path
 export default {
