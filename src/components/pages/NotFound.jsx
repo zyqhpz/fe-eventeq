75333b4118e2c2b1d7a6e45019c18740fa0f2a11
Haziq Hapiz <zyqq.dev@gmail.com>
2023-05-06 23:46:08 +0800

    add image in 404 Not Found page

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,12 +1,15 @@
 import Footer from "../ui/Footer";
 import Navbar from "../ui/Navbar";
 
+import Error from "./../../assets/error.png"
+
 export default function NotFound() {
   return (
     <div className="Home">
       <Navbar />
       <div className="bg-gray-100 w-screen h-screen">
         <div className="flex flex-col items-center justify-center h-full">
+          <img src={Error} alt="404" className="w-40 h-40" />
           <h1 className="text-4xl font-bold text-gray-800">404 Not Found</h1>
           <p className="text-gray-600">Are you missing?</p>
         </div>
