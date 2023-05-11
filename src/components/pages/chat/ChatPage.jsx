1a02120c92b2da6bc7703514acc6787999ab3b35
Haziq Hapiz <zyqq.dev@gmail.com>
2023-05-11 19:22:29 +0800

    refactor style for message section

--- src/components/pages/chat/ChatPage.jsx
+++ src/components/pages/chat/ChatPage.jsx
@@ -8,29 +8,60 @@ import Navbar from "../../ui/Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImages, faLocationArrow } from "@fortawesome/free-solid-svg-icons";
 
-
-const Message = ({ text, isSent }) => {
-  const messageClassNames = [
-    "inline-block",
-    "rounded-full",
-    "px-4",
-    "py-2",
-    isSent
-      ? "bg-blue-500 text-white float-right"
-      : "bg-gray-200 text-gray-900 float-left",
-  ].join(" ");
-
-  return <div className={messageClassNames}>{text}</div>;
-};
-
 const MessageList = ({ messages }) => (
-  <div className="flex flex-col space-y-4">
+  <div className="flex flex-col flex-nowrap	space-y-4 w-full">
     {messages &&
-      messages.map(({ id, text, isSent }) => (
-        <Message key={id} text={text} isSent={isSent} />
+      messages.map(({ id, text, sentBy, sentByID, sentAt }) => (
+        <Message
+          key={id}
+          text={text}
+          sentByID={sentByID}
+          sentBy={sentBy}
+          sentAt={sentAt}
+        />
       ))}
   </div>
 );
+
+const Message = ({ text, sentBy, sentByID, sentAt }) => {
+  const messageClassNames = [
+    "flex",
+    "rounded-xl",
+    "px-4",
+    "py-2",
+    "text-base",
+    "max-w-xs",
+    "shadow-sm",
+    sentByID === useLocation().state.ID
+      ? "bg-orange-200 text-black text-right"
+      : "bg-gray-400 text-white text-left",
+  ].join(" ");
+
+  const dateClassNames = [
+    "flex pt-1 ",
+    sentByID === useLocation().state.ID
+      ? "justify-end"
+      : "justify-start",
+  ].join(" ");
+
+  const messageBoxClassNames = [
+    "flex",
+    sentByID === useLocation().state.ID
+      ? "justify-end"
+      : "justify-start",
+  ].join(" ");
+
+  return (
+    <div className={messageBoxClassNames}>
+      <div className="grid grid-cols-1">
+        <div className={messageClassNames}>{text}</div>
+        <div className={dateClassNames}>
+          <p className="text-xs text-gray-500">{sentAt}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default function ChatPage() {
 
@@ -45,7 +76,12 @@ export default function ChatPage() {
     const newMessage = {
       id: messages.length + 1,
       text,
-      isSent: true,
+      sentAt: new Date().toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+      }),
+      sentBy: state.FirstName + " " + state.LastName,
+      sentByID: "123"
     };
 
     setMessages([...messages, newMessage]);
@@ -68,24 +104,24 @@ export default function ChatPage() {
     return (
       <div className="min-h-screen flex flex-col w-screen">
         <Navbar />
-        <div className="bg-gray-100 h-screen max-w-full">
+        <div className="bg-gray-100 h-full max-w-full">
           {/* <div className="max-w-xl">
             <h1>Chat Page</h1>
           </div> */}
           <div>
-            <h1>{state.FirstName + " " + state.LastName}</h1>
-            <div className="w-full h-32 bg-orange-400"></div>
-
-            <div className="container mx-auto -mt-32 h-screen max-w-xl">
-              <div className="py-6 h-screen">
-                <div className="flex border border-grey rounded shadow-lg h-full">
+            <div
+              className="container mx-auto -mt-38 h-screen w-screen"
+              style={{ height: "calc(100vh - 96px)" }}
+            >
+              <div className="py-2 h-full">
+                <div className="flex border border-grey rounded shadow-lg h-full w-full">
                   <div className="w-1/3 border flex flex-col">
-                    <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center">
-                      <div>
+                    <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-center items-center">
+                      {/* <div> */}
                         {/* <img className="w-10 h-10 rounded-full" src="http://andressantibanez.com/res/avatar.png"/> */}
-                      </div>
+                      {/* </div> */}
 
-                      <div className="flex"></div>
+                      {/* <div className="flex"></div> */}
                     </div>
 
                     <div className="py-2 px-2 bg-grey-lightest">
@@ -105,9 +141,6 @@ export default function ChatPage() {
                         </div>
                         <div className="ml-4">
                           <p className="text-grey-darkest">User Name</p>
-                          <p className="text-grey-darker text-xs mt-1">
-                            Andrés, Tom, Harrison, Arnold, Sylvester
-                          </p>
                         </div>
                       </div>
 
@@ -129,16 +162,16 @@ export default function ChatPage() {
                       </div>
                     </div>
 
-                    <div className="flex-1 overflow-auto bg-orange-200">
-                      <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center">{
-                        messages && messages.length > 0 ? (
+                    <div className="flex-1 overflow-auto bg-gray-200">
+                      <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center">
+                        {messages && messages.length > 0 ? (
                           <MessageList messages={messages} />
                         ) : (
-                          <div className="text-center text-gray-500">
-                            No messages yet
+                          <div className="mx-auto text-center text-gray-500">
+                            Start a conversation
                           </div>
-                        )
-                      }</div>
+                        )}
+                      </div>
                     </div>
 
                     <div className="bg-grey-lighter px-4 py-4 flex items-center">
@@ -172,7 +205,7 @@ export default function ChatPage() {
             </div>
           </div>
         </div>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     );
 }
