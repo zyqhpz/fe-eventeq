6309e82ac50d940b993471026920c16a49493d00
Haziq Hapiz <zyqq.dev@gmail.com>
2023-06-04 01:12:19 +0800

    clean code

--- src/components/pages/chat/ChatPage.jsx
+++ src/components/pages/chat/ChatPage.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef }from "react";
 
 import { useLocation } from "react-router-dom";
 
-import Footer from "../../ui/Footer";
 import Navbar from "../../ui/Navbar";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -228,107 +227,106 @@ export default function ChatPage() {
     );
   };
 
-    return (
-      <div className="min-h-screen flex flex-col w-screen">
-        <Navbar />
-        <div className="bg-gray-100 h-full max-w-full">
-          <div>
-            <div
-              className="container mx-auto -mt-38 h-screen w-screen"
-              style={{ height: "calc(100vh - 96px)" }}
-            >
-              <div className="py-2 h-full">
-                <div className="flex border border-grey rounded shadow-lg h-full w-full">
+  return (
+    <div className="min-h-screen flex flex-col w-screen">
+      <Navbar />
+      <div className="bg-gray-100 h-full max-w-full">
+        <div>
+          <div
+            className="container mx-auto -mt-38 h-screen w-screen"
+            style={{ height: "calc(100vh - 96px)" }}
+          >
+            <div className="py-2 h-full">
+              <div className="flex border border-grey rounded shadow-lg h-full w-full">
 
-                  {/* LEFT SECTION */}
-                  <div className="w-1/3 border flex flex-col">
-                    <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-center items-center w-full">
-                      <div className="flex">
-                        <p className="text-grey-darkest">Contacts</p>
+                {/* LEFT SECTION */}
+                <div className="w-1/3 border flex flex-col">
+                  <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-center items-center w-full">
+                    <div className="flex">
+                      <p className="text-grey-darkest">Contacts</p>
+                    </div>
+                  </div>
+
+                  <div className="py-2 px-2 bg-grey-lightest w-full">
+                    <input
+                      type="text"
+                      className="w-full px-2 py-2 text-sm"
+                      placeholder="Search or start new chat"
+                    />
+                    <div className="w-full mt-6">
+                      {loadingUsers ? (
+                        <div className="flex justify-center">
+                          <LoadingButton />
+                        </div>
+                      ) : (
+                        <UsersList users={users} />
+                      )}
+                    </div>
+                  </div>
+                </div>
+
+                {/* RIGHT SECTION*/}
+                <div className="w-2/3 border flex flex-col">
+                  <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center">
+                    <div className="flex items-center">
+                      <div className="ml-4">
+                        <p className="text-grey-darkest">{isHavingConversation ? secondUser.FirstName + " " + secondUser.LastName : "No conversation selected"}</p>
                       </div>
                     </div>
-
-                    <div className="py-2 px-2 bg-grey-lightest w-full">
-                      <input
-                        type="text"
-                        className="w-full px-2 py-2 text-sm"
-                        placeholder="Search or start new chat"
-                      />
-                      <div className="w-full mt-6">
-                        {loadingUsers ? (
-                          <div className="flex justify-center">
-                            <LoadingButton />
-                          </div>
-                        ) : (
-                          <UsersList users={users} />
-                        )}
+                    <div className="flex">
+                      <div className="ml-6">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          width="24"
+                          height="24"
+                        >
+                          <path
+                            fill="#263238"
+                            fillOpacity=".6"
+                            d="M12 7a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 7zm0 2a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 9zm0 6a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 15z"
+                          ></path>
+                        </svg>
                       </div>
                     </div>
                   </div>
 
-                  {/* RIGHT SECTION*/}
-                  <div className="w-2/3 border flex flex-col">
+                  <div className="flex-1 overflow-auto bg-gray-200">
                     <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center">
-                      <div className="flex items-center">
-                        <div className="ml-4">
-                          <p className="text-grey-darkest">{isHavingConversation ? secondUser.FirstName + " " + secondUser.LastName : "No conversation selected"}</p>
+                      {messages && messages.length > 0 ? (
+                        <MessageList messages={messages} />
+                      ) : (
+                        <div className="mx-auto text-center text-gray-500">
+                          Start a conversation
                         </div>
-                      </div>
-                      <div className="flex">
-                        <div className="ml-6">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            width="24"
-                            height="24"
-                          >
-                            <path
-                              fill="#263238"
-                              fillOpacity=".6"
-                              d="M12 7a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 7zm0 2a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 9zm0 6a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 15z"
-                            ></path>
-                          </svg>
-                        </div>
-                      </div>
+                      )}
                     </div>
+                  </div>
 
-                    <div className="flex-1 overflow-auto bg-gray-200">
-                      <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center">
-                        {messages && messages.length > 0 ? (
-                          <MessageList messages={messages} />
-                        ) : (
-                          <div className="mx-auto text-center text-gray-500">
-                            Start a conversation
-                          </div>
-                        )}
-                      </div>
-                    </div>
-
-                    <div className="bg-grey-lighter px-4 py-4 flex items-center">
-                      <FontAwesomeIcon icon={faImages} />
-                      <div className="flex-1 mx-4">
-                        <input
-                          className="w-full border rounded px-2 py-2"
-                          type="text"
-                          name="message"
-                          id="message-input"
-                          autoComplete="off"
-                          value={message}
-                          onChange={(event) => setMessage(event.target.value)}
-                          onKeyDown={(event) => {
-                            if (event.key === "Enter") {
-                              handleSubmit(event);
-                            }
-                          }}
-                        />
-                      </div>
-                      <FontAwesomeIcon
-                        icon={faLocationArrow}
-                        className="rotate-45 cursor-pointer text-black hover:text-orange-400"
-                        onClick={handleSubmit}
-                        id="send-message"
+                  <div className="bg-grey-lighter px-4 py-4 flex items-center">
+                    <FontAwesomeIcon icon={faImages} />
+                    <div className="flex-1 mx-4">
+                      <input
+                        className="w-full border rounded px-2 py-2"
+                        type="text"
+                        name="message"
+                        id="message-input"
+                        autoComplete="off"
+                        value={message}
+                        onChange={(event) => setMessage(event.target.value)}
+                        onKeyDown={(event) => {
+                          if (event.key === "Enter") {
+                            handleSubmit(event);
+                          }
+                        }}
                       />
                     </div>
+                    <FontAwesomeIcon
+                      icon={faLocationArrow}
+                      className="rotate-45 cursor-pointer text-black hover:text-orange-400"
+                      onClick={handleSubmit}
+                      id="send-message"
+                    />
                   </div>
                 </div>
               </div>
@@ -336,5 +334,6 @@ export default function ChatPage() {
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
