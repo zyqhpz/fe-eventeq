c375f9d31ce5c02759aa96f8c7ac3b0643b84edf
Haziq Hapiz <zyqq.dev@gmail.com>
2023-06-05 23:15:08 +0800

    add booking panel in booking page

--- src/components/pages/BookingItem.jsx
+++ src/components/pages/BookingItem.jsx
@@ -15,6 +15,7 @@ export default function BookingItem () {
   const [owner, setOwner] = useState(null)
   const [items, setItems] = useState([])
   const [loading, setLoading] = useState(true)
+  const [bookingItems, setBookingItems] = useState([])
 
   const backPath = '/listing/item/' + tempItemId
 
@@ -36,30 +37,65 @@ export default function BookingItem () {
     <div className="p-8 flex flex-col w-screen">
       <ul className="menu menu-horizontal bg-orange-500 text-white hover:bg-base-200 hover:text-black rounded-box w-24">
         <li>
-          <Link to={backPath} className="btn btn-ghost btn-sm rounded-btn" onClick={removeTempItemId}>
+          <Link
+            to={backPath}
+            className="btn btn-ghost btn-sm rounded-btn"
+            onClick={removeTempItemId}
+          >
             <FontAwesomeIcon icon={faArrowLeft} />
             Back
           </Link>
         </li>
       </ul>
-      <div className="mt-4">
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-        <div>
-          <div>
-          <h1 className="text-3xl font-bold">List of Items Provided by {owner.FirstName + ' ' + owner.LastName}</h1>
-          This is BookingItem page for owner {ownerId} and user {userId}
-          </div>
-          <div className="flex flex-wrap">
-            <div className="flex flex-col w-2/4 p-4">
-            {items.map((item) => (
-              <ItemBookingCard key={item.ID} item={item} />
-            ))}
+      {/* Main page */}
+      <div className="mt-4 flex flex-row w-full">
+        {/* Left side */}
+        <div className='w-2/3'>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <div className='w-full'>
+              <div>
+                <h1 className="text-3xl font-bold">
+                  List of Items Provided by{' '}
+                  {owner.FirstName + ' ' + owner.LastName}
+                </h1>
+                This is BookingItem page for owner {ownerId} and user {userId}
+              </div>
+              <div className="flex flex-wrap">
+                <div className="flex flex-col w-3/4 p-4">
+                  {items.map((item) => (
+                    <ItemBookingCard key={item.ID} item={item} />
+                  ))}
+                </div>
+              </div>
             </div>
+          )}
         </div>
+        {/* right side */}
+        <div className='w-1/3'>
+          <h1 className="text-3xl font-bold">
+            Booking Summary
+          </h1>
+          <div className="flex flex-col w-full p-4">
+            <div className="flex flex-row justify-between">
+              <p className="text-xl font-bold">Item Name 1</p>
+              <p className="text-xl font-bold">2</p>
+              <p className="text-xl font-bold">RM 0.00</p>
+            </div>
+            <div className="flex flex-row justify-between">
+              <p className="text-xl font-bold">Total</p>
+              <p className="text-xl font-bold">RM 0.00</p>
+            </div>
+            <div className="flex flex-row justify-between">
+              <p className="text-xl font-bold">Deposit</p>
+              <p className="text-xl font-bold">RM 0.00</p>
+            </div>
+            <div className="flex flex-row justify-between">
+
+            </div>
+          </div>
         </div>
-        )}
       </div>
     </div>
   )
