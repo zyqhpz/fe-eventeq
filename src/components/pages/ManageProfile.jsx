8dfcdc6ecf0bc70faebc51d99c668a64e865b97c
Haziq Hapiz <zyqq.dev@gmail.com>
2023-07-04 03:53:36 +0800

    clean manage profile

--- src/components/pages/ManageProfile.jsx
+++ src/components/pages/ManageProfile.jsx
@@ -251,7 +251,7 @@ export default function ManageProfile () {
       description: 'Your profile has been updated successfully.',
       status: 'success',
       position: 'top-right',
-      duration: 9000,
+      duration: 3000,
       isClosable: true
     })
   }
@@ -262,7 +262,6 @@ export default function ManageProfile () {
     fetch(path.url + 'api/user/id/' + userId)
       .then((res) => res.json())
       .then((data) => {
-        // setUser(data)
         setFirstName(data.FirstName)
         setLastName(data.LastName)
         setEmail(data.Email)
@@ -332,9 +331,6 @@ export default function ManageProfile () {
       .then((response) => {
         if (response.data.status === 'success') {
           UpdateSuccess()
-          setTimeout(() => {
-            window.location.reload()
-          }, 2000)
         }
       })
       .catch((error) => {
@@ -342,42 +338,6 @@ export default function ManageProfile () {
       })
     setLoading(false)
   }
-
-  // const compressImage = (file) => {
-  //   return new Promise((resolve) => {
-  //     const reader = new FileReader()
-  //     reader.readAsDataURL(file)
-  //     reader.onload = (event) => {
-  //       const img = new Image()
-  //       img.src = event.target.result
-  //       img.onload = () => {
-  //         const canvas = document.createElement('canvas')
-  //         const ctx = canvas.getContext('2d')
-  //         const MAX_WIDTH = 800
-  //         let width = img.width
-  //         let height = img.height
-  //         if (width > MAX_WIDTH) {
-  //           height *= MAX_WIDTH / width
-  //           width = MAX_WIDTH
-  //         }
-  //         canvas.width = width
-  //         canvas.height = height
-  //         ctx.drawImage(img, 0, 0, width, height)
-  //         canvas.toBlob(
-  //           (blob) => {
-  //             const compressedFile = new File([blob], file.name, {
-  //               type: file.type,
-  //               lastModified: Date.now()
-  //             })
-  //             resolve(compressedFile)
-  //           },
-  //           file.type,
-  //           0.7
-  //         )
-  //       }
-  //     }
-  //   })
-  // }
 
   return (
     <div>
