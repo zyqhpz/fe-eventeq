aa31a02680334e39d914c5cc473104266d2a3d2e
Haziq Hapiz <zyqq.dev@gmail.com>
2023-07-06 02:44:14 +0800

    refactor profile navbar when not auth

--- src/components/ui/ProfileIconButton.jsx
+++ src/components/ui/ProfileIconButton.jsx
@@ -69,31 +69,45 @@ export default function Example () {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex w-full items-center justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-
-          {loading ? (
-            <p className="text-gray-900"></p>
-          ) : (
-            <>
-              {
-                user.IsAvatarImageSet ? (
+        {isAuthenticated === false ? (
+          <Menu.Button className="inline-flex w-full items-center justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+            <FontAwesomeIcon icon={faCircleUser} className="w-4 h-4" />
+            <span>Profile</span>
+            <ChevronDownIcon
+              className="-mr-1 h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+          </Menu.Button>
+        ) : (
+          <Menu.Button className="inline-flex w-full items-center justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+            {loading ? (
+              <p className="text-gray-900"></p>
+            ) : (
+              user.IsAvatarImageSet ? (
+                <>
                   <img
                     src={path.url + 'api/item/image/' + user.ProfileImage}
                     alt=""
                     className="w-4 h-4 md:w-5 md:h-5 rounded-full object-cover"
                   />
-                ) : (
-                  <FontAwesomeIcon icon={faCircleUser} className="w-4 h-4" />
-                )
-              }
-              <span>{name}</span>
-            </>
-          )}
-          <ChevronDownIcon
-            className="-mr-1 h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-        </Menu.Button>
+                  <span>{name}</span>
+                </>
+              ) : (
+                <>
+                  <FontAwesomeIcon
+                    icon={faCircleUser}
+                    className="w-4 h-4"
+                  />
+                  <span>{name}</span>
+                </>
+              )
+            )}
+            <ChevronDownIcon
+              className="-mr-1 h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+          </Menu.Button>
+        )}
       </div>
 
       <Transition
@@ -206,7 +220,7 @@ export default function Example () {
                 </a>
               )}
             </Menu.Item>
-            <hr/>
+            <hr />
             <Menu.Item className={isAuthenticated ? 'block' : 'hidden'}>
               {({ active }) => (
                 <button
