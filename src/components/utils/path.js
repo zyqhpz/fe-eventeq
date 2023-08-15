0c9baef83f0957c0860b1d7a74412c476a390380
Haziq Hapiz <zyqq.dev@gmail.com>
2023-08-15 13:39:02 +0800

    clean the solution

--- src/components/utils/path.js
+++ src/components/utils/path.js
@@ -1,6 +1,6 @@
 // if ifProduction is true, then the path will be the production path
 // if ifProduction is false, then the path will be the development path
-const isProduction = false
+const isProduction = true
 
 const path = {
   get url () {
@@ -10,7 +10,7 @@ const path = {
   },
   get ws () {
     return isProduction
-      ? 'ws://be-eventeq-production.up.railway.app/'
+      ? 'wss://be-eventeq-production.up.railway.app/'
       : 'ws://localhost:8080/'
   }
 }
