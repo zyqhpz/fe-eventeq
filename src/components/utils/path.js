5381b412a83bcf0e4c1a0fdd4f168b71357e1ede
Haziq Hapiz <zyqq.dev@gmail.com>
2023-08-19 01:23:42 +0800

    Update path.js have AWS Lightsail

--- src/components/utils/path.js
+++ src/components/utils/path.js
@@ -1,6 +1,6 @@
 // if ifProduction is true, then the path will be the production path
 // if ifProduction is false, then the path will be the development path
-const isProduction = false
+const isProduction = true
 
 const path = {
   get url () {
