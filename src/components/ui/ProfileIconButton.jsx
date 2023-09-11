d983e4d931a58e92a61dde7ef6171bd70d401859
Haziq Hapiz <zyqq.dev@gmail.com>
2023-09-11 19:18:48 +0800

    remove blue indicator in message navbar

--- src/components/ui/ProfileIconButton.jsx
+++ src/components/ui/ProfileIconButton.jsx
@@ -180,10 +180,6 @@ export default function Example () {
                   )}
                 >
                   Message
-                  <span className="relative flex h-3 w-3">
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
-                  </span>
                 </Link>
               )}
             </Menu.Item>
