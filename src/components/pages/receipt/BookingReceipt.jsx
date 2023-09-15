89a8e7bca108236889b1c42289b9bd774768f6c6
Haziq Hapiz <zyqq.dev@gmail.com>
2023-09-16 02:01:13 +0800

    finalize the receipt UI

--- src/components/pages/receipt/BookingReceipt.jsx
+++ src/components/pages/receipt/BookingReceipt.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Page,
   Text,
@@ -10,6 +10,8 @@ import {
 } from '@react-pdf/renderer'
 
 import EventEQLogo from '../../../../src/assets/EventEQ.png'
+
+import path from '../../utils/path'
 
 const tailwindStyles = {
   textBase: { fontSize: 12 }, // Tailwind's base text size
@@ -92,8 +94,8 @@ const styles = StyleSheet.create({
   table: {
     display: 'table',
     width: '100%',
-    margin: 'auto',
-    fontSize: 12
+    fontSize: 12,
+    marginBottom: 10
   },
   tableRow: {
     flexDirection: 'row'
@@ -103,6 +105,14 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#000',
     padding: 5
+  },
+  rightSection: {
+    flex: 1,
+    textAlign: 'right',
+    fontSize: 12
+  },
+  rightContainer: {
+    width: '100%' // Adjust the width as needed
   },
   totalRow: {
     flexDirection: 'row',
@@ -130,88 +140,120 @@ const styles = StyleSheet.create({
   }
 })
 
-const MyDocument = () => {
+export default function BookingReceipt () {
   // Extract the bookingId from the URL
   const pathParts = window.location.pathname.split('/')
   const bookingId = pathParts[pathParts.indexOf('booking') + 1]
 
-  // Sample data
-  const fromWhom = 'Your Company'
-  const toWhom = 'Customer Name'
-  const items = [
-    { itemName: 'Item 1', quantity: 2, price: 50, total: 0 },
-    { itemName: 'Item 2', quantity: 1, price: 30, total: 0 }
-  ]
+  const [data, setData] = useState({})
 
-  // calculate the total price of all items and update in the items array
-  items.forEach((item) => {
-    item.total = item.quantity * item.price
-  })
-
-  const grandTotal = items.reduce(
-    (total, item) => total + item.quantity * item.price,
-    0
-  )
+  useEffect(() => {
+    const getData = async () => {
+      fetch(
+        path.url + 'api/booking/receipt/' + bookingId
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          if (data !== null) {
+            console.log(data)
+            setData(data)
+          }
+        })
+    }
+    getData()
+  }, [bookingId])
 
   return (
-    <Document>
+    <Document
+      title="EventEQ Booking Receipt"
+      subject={'EventEQ-Booking-' + bookingId}
+    >
       <Page size="A4" style={styles.page}>
         <View style={styles.section}>
           {/* Logo */}
           <View style={styles.logoContainer}>
             <Image src={EventEQLogo} style={styles.logo} />
-            <Text style={styles.title}>Invoice</Text>
+            <Text style={styles.title}>Receipt</Text>
           </View>
 
-          <View
-            style={[
-              {
-                display: 'flex',
-                flexDirection: 'column',
-                textAlign: 'left',
-                fontSize: 12,
-                marginBottom: 10,
-                marginTop: 10
-              }
-            ]}
-          >
-            <Text>
-              <Text
-                style={{
-                  fontWeight: 'bold'
-                }}
-              >
-                Booking ID:
-              </Text>{' '}
-              {bookingId}
-            </Text>
-            <Text>
-              <Text
-                style={{
-                  fontWeight: 'bold'
-                }}
-              >
-                Date:
-              </Text>{' '}
-              12/12/2020
-            </Text>
-          </View>
+          {data && data.Booking ? (
+            <View
+              style={[
+                {
+                  display: 'flex',
+                  flexDirection: 'column',
+                  textAlign: 'left',
+                  fontSize: 12,
+                  marginBottom: 10,
+                  marginTop: 10
+                }
+              ]}
+            >
+              {/* <Text>
+                <Text
+                  style={{
+                    fontWeight: 'bold'
+                  }}
+                >
+                  Booking ID:
+                </Text>{' '}
+                {data.Booking.Id}
+              </Text> */}
+              <Text>
+                <Text
+                  style={{
+                    fontWeight: 'bold'
+                  }}
+                >
+                  Created Date:
+                </Text>{' '}
+                {new Date(data.Booking.CreatedAt).toLocaleString('en-MY', {
+                  timeZone: 'Asia/Kuala_Lumpur',
+                  day: '2-digit',
+                  month: '2-digit',
+                  year: 'numeric'
+                })}
+              </Text>
+              <Text>
+                <Text
+                  style={{
+                    fontWeight: 'bold'
+                  }}
+                >
+                  Booking Date:
+                </Text>{' '}
+                {data.Booking.StartDate}
+                {' - '}
+                {data.Booking.EndDate}
+              </Text>
+            </View>
+          ) : (
+            <Text>No booking found</Text>
+          )}
 
           {/* From and To Info */}
-          <View style={styles.infoContainer}>
-            <View style={styles.infoText}>
-              <Text style={styles.header}>From:</Text>
-              <Text style={tailwindStyles.textSm}>{fromWhom}</Text>
-              <Text style={tailwindStyles.textSm}>Email</Text>
-              <Text style={tailwindStyles.textSm}>No. Phone</Text>
+          {data && data.Owner && data.Renter ? (
+            <View style={styles.infoContainer}>
+              <View style={styles.infoText}>
+                <Text style={styles.header}>From:</Text>
+                <Text style={tailwindStyles.textSm}>
+                  {data.Owner.FirstName} {data.Owner.LastName}
+                </Text>
+                <Text style={tailwindStyles.textSm}>{data.Owner.Email}</Text>
+                <Text style={tailwindStyles.textSm}>{data.Owner.NoPhone}</Text>
+              </View>
+              <View style={styles.infoText}>
+                <Text style={styles.header}>To:</Text>
+                <Text style={tailwindStyles.textSm}>
+                  {data.Renter.FirstName} {data.Renter.LastName}
+                </Text>
+                <Text style={tailwindStyles.textSm}>{data.Renter.Email}</Text>
+                <Text style={tailwindStyles.textSm}>{data.Renter.NoPhone}</Text>
+              </View>
             </View>
-            <View style={styles.infoText}>
-              <Text style={styles.header}>To:</Text>
-              <Text style={tailwindStyles.textSm}>{toWhom}</Text>
-              <Text style={tailwindStyles.textSm}>Email</Text>
-              <Text style={tailwindStyles.textSm}>No. Phone</Text>
-            </View>
-          </View>
+          ) : (
+            <Text>No owner or renter found</Text>
+          )}
 
           {/* Table for Details */}
           <View style={styles.table}>
@@ -232,47 +274,67 @@ const MyDocument = () => {
             </View>
 
             {/* Table Rows */}
-            {items.map((item, index) => (
-              <View style={styles.tableRow} key={index}>
-                <View style={styles.tableCell}>
-                  <Text>{item.itemName}</Text>
+            {data && data.Booking && data.Booking.Items ? (
+              data.Booking.Items.map((item, index) => (
+                <View style={styles.tableRow} key={index}>
+                  <View style={styles.tableCell}>
+                    <Text>{item.Name}</Text>
+                  </View>
+                  <View style={styles.tableCell}>
+                    <Text>{item.Quantity}</Text>
+                  </View>
+                  <View style={styles.tableCell}>
+                    <Text>
+                      RM&nbsp;
+                      {parseFloat(item.Price).toFixed(2)}
+                    </Text>
+                  </View>
+                  <View style={styles.tableCell}>
+                    <Text>
+                      RM&nbsp;
+                      {parseFloat(item.Quantity * item.Price).toFixed(2)}
+                    </Text>
+                  </View>
                 </View>
-                <View style={styles.tableCell}>
-                  <Text>{item.quantity}</Text>
-                </View>
-                <View style={styles.tableCell}>
-                  <Text>
-                    RM&nbsp;
-                    {
-                      // Format the price to 2 decimal places
-                      parseFloat(item.price).toFixed(2)
-                    }
+              ))
+            ) : (
+              <Text>No items found</Text>
+            )}
+          </View>
+
+          {data && data.Booking ? (
+            <View style={styles.rightSection}>
+              {/* Enclose the right section in a fixed-width container */}
+              <View style={styles.rightContainer}>
+                {/* Sub Total */}
+                <View style={styles.totalRow}>
+                  <Text style={styles.totalLabel}>Sub Total:</Text>
+                  <Text style={styles.totalValue}>
+                    RM {parseFloat(data.Booking.SubTotal).toFixed(2)}
                   </Text>
                 </View>
-                <View style={styles.tableCell}>
-                  <Text>
-                    RM&nbsp;
-                    {
-                      // Format the total to 2 decimal places
-                      parseFloat(item.total).toFixed(2)
-                    }
+                {/* Service Fee */}
+                <View style={styles.totalRow}>
+                  <Text style={styles.totalLabel}>Service Fee (7%):</Text>
+                  <Text style={styles.totalValue}>
+                    RM {parseFloat(data.Booking.ServiceFee).toFixed(2)}
+                  </Text>
+                </View>
+                {/* Grand Total */}
+                <View style={styles.totalRow}>
+                  <Text style={styles.totalLabel}>Grand Total:</Text>
+                  <Text style={styles.totalValue}>
+                    RM {parseFloat(data.Booking.GrandTotal).toFixed(2)}
                   </Text>
                 </View>
               </View>
-            ))}
-          </View>
+            </View>
+          ) : null}
 
-          {/* Total */}
-          <View style={styles.totalRow}>
-            <Text style={styles.totalLabel}>Total:</Text>
-            <Text style={styles.totalValue}>{grandTotal}</Text>
-          </View>
           {/* Thank You */}
-          <Text style={styles.thankYou}>Thank you for choosing us!</Text>
+          <Text style={styles.thankYou}>Thank you for choosing EventEQ.</Text>
         </View>
       </Page>
     </Document>
   )
 }
-
-export default MyDocument
