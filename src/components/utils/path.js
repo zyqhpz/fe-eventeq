dcfed8431d63620e30926a45481e531136de912c
Haziq Hapiz <zyqq.dev@gmail.com>
2023-10-10 17:16:20 +0800

    update api path to Railway

--- src/components/utils/path.js
+++ src/components/utils/path.js
@@ -4,18 +4,19 @@ const isProduction = true
 
 const LOCAL = import.meta.env.VITE_PATH_LOCAL
 const DOCKER = import.meta.env.VITE_PATH_DOCKER
+const RAILWAY = import.meta.env.VITE_PATH_RAILWAY
 
 // const RAILWAY = process.env.PATH_RAILWAY
 
 const path = {
   get url () {
     return isProduction
-      ? 'https://' + DOCKER
+      ? 'https://' + RAILWAY
       : 'http://' + LOCAL
   },
   get ws () {
     return isProduction
-      ? 'wss://' + DOCKER
+      ? 'wss://' + RAILWAY
       : 'ws://' + LOCAL
   }
 }
